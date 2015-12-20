c69defdff5834fcbf1cde5133ceae8036b78a4c7
Vlad <pogoriliy13@mail.ru>
2015-12-20 11:31:56 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import javax.xml.soap.SOAPPart;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        /*System.out.println("Enter Your name: ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Hi, "+name+" !");
+        scan.close();
+        */
+        byte b1=50, b2=-99;
+        b1+=b2;
+        System.out.println(+b1);
+        // write your code herea
+    }
+}
